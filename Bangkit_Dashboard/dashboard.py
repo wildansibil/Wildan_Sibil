6ae41882c890e5f938ee6b1856d288af34f050e1
wildansibil <wildansibil2@gmail.com>
2024-10-06 16:54:43 +0700

    Update dashboard.py

--- Bangkit_Dashboard/dashboard.py
+++ Bangkit_Dashboard/dashboard.py
@@ -230,9 +230,4 @@ ax.set_ylabel('Jumlah Pengguna Sepeda', fontsize=14)
 
 # Menampilkan plot di Streamlit
 st.pyplot(fig)
-
 st.caption('Copyright (c) Wildan Sibil Danillah Safri 2024')
-
-# Petunjuk untuk menjalankan dashboard
-# Untuk menjalankan dashboard ini, gunakan perintah berikut di terminal:
-# streamlit run Bangkit_Dashboard/dashboard.py
